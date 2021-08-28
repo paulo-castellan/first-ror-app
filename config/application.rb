8e29c57da10272753ffac8f5aa6e775098a33830
paulo-castellan <paulo.castellan@gmail.com>
2021-08-28 16:08:18 -0300

    task H - add React

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#config.filter_parameters += [ :credit_card_number ]
+
 module Depot601
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
